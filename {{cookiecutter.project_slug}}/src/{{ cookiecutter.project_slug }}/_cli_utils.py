fe13d73602c0df18b31a8d137abdbb8988d85221
Stephen Richer <sr467@bath.ac.uk>
2023-04-07 22:42:29 +0100

    Update command line structure.

--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}/_cli_utils.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}/_cli_utils.py
@@ -2,36 +2,143 @@
 
 """ Helper functions for CLI """
 
-import logging
+import sys
 import argparse
-from timeit import default_timer
+from datetime import datetime
 
 
-def _executeCommand(args):
-    """ Handle function calls after argument parsing """
-    # Initialise logging
-    logFormat = '%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
-    logging.basicConfig(level=args.verbose, format=logFormat)
-    del args.verbose
-    {%- if cookiecutter.command_line_interface|lower == 'yes (with subcommand)' %}
-    del args.command
-    {%- endif %}
-    # Pop main function and excute script
-    function = args.__dict__.pop('function')
-    start = default_timer()
-    rc = function(**vars(args))
-    end = default_timer()
-    logging.info(f'Total execution time: {end - start:.3f} seconds.')
-    logging.shutdown()
-    return rc
+class OptionsParser():
+    def __init__(self, argv: list[str] | None = None,
+                 epilog: str = '', version: str = ''):
+        self.argv = argv
+        self.epilog = epilog
+        self.base_parser = self._get_base_parser(version)
+
+    def prepare_subparser(self) -> tuple[argparse.ArgumentParser, argparse._SubParsersAction]:
+        parser = self.prepare_parser()
+        subparser = self._get_subparser(parser)
+        self.subparser = True
+        return parser, subparser
+
+    def prepare_parser(self) -> argparse.ArgumentParser:
+        """ Create base parser of verbose/version. """
+        parser = argparse.ArgumentParser(
+            epilog=self.epilog, description=__doc__,
+            parents=[self.base_parser])
+        return parser
+
+    def _get_base_parser(self, version: str) -> argparse.ArgumentParser:
+        """ Create base parser of verbose/version. """
+        base_parser = argparse.ArgumentParser(add_help=False)
+        base_parser.add_argument(
+            '--version', action='version', version=f'%(prog)s {version}')
+        base_parser.add_argument(
+            '--verbose', action='count',
+            default=0, help='verbose logging for debugging')
+        return base_parser
+
+    def _get_subparser(self, parser: argparse.ArgumentParser
+                       ) -> argparse._SubParsersAction:
+        subparser = parser.add_subparsers(
+            title='required commands',
+            description='',
+            dest='command',
+            metavar='Commands',
+            help='Description:')
+        return subparser
+
+    def _validate_subcommand(self, args: argparse.Namespace,
+                             parser: argparse.ArgumentParser) -> None:
+        if (self.subparser) and ('function' not in args):
+            parser.print_help()
+            sys.exit()
 
 
-def _getBaseParser(version: str) -> argparse.Namespace:
-    """ Create base parser of verbose/version. """
-    parser = argparse.ArgumentParser(add_help=False)
-    parser.add_argument(
-        '--version', action='version', version='%(prog)s {}'.format(version))
-    parser.add_argument(
-        '--verbose', action='store_const', const=logging.DEBUG,
-        default=logging.ERROR, help='verbose logging for debugging')
-    return parser
+class IntRange:
+    def __init__(self, imin: int | None = None, imax: int | None = None):
+        self.imin = imin
+        self.imax = imax
+
+    def __call__(self, arg: str):
+        try:
+            value = int(arg)
+        except ValueError:
+            raise self.exception()
+        if ((self.imin is not None and value < self.imin)
+                or (self.imax is not None and value > self.imax)):
+            raise self.exception()
+        return value
+
+    def exception(self):
+        if self.imin is not None and self.imax is not None:
+            return argparse.ArgumentTypeError(
+                f'Must be an integer in the range [{self.imin}, {self.imax}].')
+        elif self.imin is not None:
+            return argparse.ArgumentTypeError(
+                f'Must be an integer >= {self.imin}.')
+        elif self.imax is not None:
+            return argparse.ArgumentTypeError(
+                f'Must be an integer <= {self.imax}.')
+        else:
+            return argparse.ArgumentTypeError('Must be an integer.')
+
+
+class FloatRange:
+    def __init__(self, fmin: int | None = None, fmax: int | None = None,
+                 inclusive: str = 'both'):
+        assert inclusive in ['both', 'neither', 'left', 'right']
+        self.fmin = fmin
+        self.fmax = fmax
+        self.inclusive = inclusive
+
+    def __call__(self, arg: str):
+        try:
+            value = float(arg)
+        except ValueError:
+            raise self.exception()
+        if self.fmin is not None:
+            if (self.inclusive in ['both', 'left']):
+                if value < self.fmin:
+                    raise self.exception()
+            elif value <= self.fmin:
+                raise self.exception()
+        if self.fmax is not None:
+            if (self.inclusive in ['both', 'right']):
+                if value > self.fmax:
+                    raise self.exception()
+            elif value >= self.fmax:
+                raise self.exception()
+        return value
+
+    def exception(self):
+        if self.fmin is not None and self.fmax is not None:
+            l = '[' if self.inclusive in ['both', 'left'] else '('
+            r = ']' if self.inclusive in ['both', 'right'] else ')'
+            return argparse.ArgumentTypeError(
+                f'Must be an float in the range {l}{self.fmin}, {self.fmax}{r}.')
+        elif self.fmin is not None:
+            op = '>=' if self.inclusive in ['both', 'left'] else '>'
+            return argparse.ArgumentTypeError(
+                f'Must be an float {op} {self.fmin}.')
+        elif self.fmax is not None:
+            op = '<=' if self.inclusive in ['both', 'right'] else '<'
+            return argparse.ArgumentTypeError(
+                f'Must be an float {op} {self.fmax}.')
+        else:
+            return argparse.ArgumentTypeError('Must be a float.')
+
+
+class Date:
+    def __init__(self, format: str = '%d-%m-%Y'):
+        self.format = format
+
+    def __call__(self, arg: str):
+        try:
+            value = datetime.strptime(arg, self.format)
+        except ValueError:
+            raise self.exception(arg)
+        return value
+
+    def exception(self, arg: str):
+        return argparse.ArgumentTypeError(
+            f"Time data '{arg}' does not match format '{self.format}'.")
--- {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}/cli.py
+++ {{cookiecutter.project_slug}}/src/{{ cookiecutter.project_slug }}/cli.py
@@ -4,64 +4,85 @@
 
 
 import sys
-import argparse
+import logging
+from timeit import default_timer
 from {{ cookiecutter.project_slug }} import __version__
-from ._cli_utils import _executeCommand, _getBaseParser
+from ._cli_utils import OptionsParser, IntRange
 {%- if cookiecutter.command_line_interface|lower == 'yes (with subcommand)' %}
 from .{{ cookiecutter.project_slug }} import prime, fibonacci
 {% else %}
 from .{{ cookiecutter.project_slug }} import prime
 {% endif %}
 
-def parseArgs() -> argparse.Namespace:
-    epilog = '{{ cookiecutter.full_name.replace('\"', '\\\"') }}, ({{ cookiecutter.email }})'
-    baseParser = _getBaseParser(__version__)
-    parser = argparse.ArgumentParser(
-        epilog=epilog, description=__doc__, parents=[baseParser])
-    {% if cookiecutter.command_line_interface|lower == 'yes (with subcommand)' %}
-    subparser = parser.add_subparsers(
-        title='required commands',
-        description='',
-        dest='command',
-        metavar='Commands',
-        help='Description:')
-    # HINT: Add your each subcommand and arguments to a new subparser.
-    # HINT: Refer to https://docs.python.org/3/library/argparse.html
-    sp1 = subparser.add_parser(
-        'prime',
-        description=prime_cli.__doc__.split('\n')[0],
-        help='Check if a number is prime.',
-        parents=[baseParser],
-        epilog=parser.epilog)
-    # HINT: Add (default: %(default)s)') to help message if you have a default.
-    sp1.add_argument('number', type=int, help='A number to test primality.')
-    # HINT: For each subcommand, define an entry-point function.
-    sp1.set_defaults(function=prime_cli)
+class CLIParser(OptionsParser):
+    def __init__(self, argv: list[str] | None = None,
+                 epilog: str = '', version: str = ''):
+        OptionsParser.__init__(self, argv, epilog, version)
 
-    # HINT: Create a new sub-parser with disctinct arguments.
-    sp2 = subparser.add_parser(
-        'fib',
-        description=fibonacci_cli.__doc__.split('\n')[0],
-        help='Check if a number is part of the fibonacci sequence.',
-        parents=[baseParser],
-        epilog=parser.epilog)
-    sp2.add_argument('number', type=int, help='A number to check.')
-    sp2.set_defaults(function=fibonacci_cli)
-    {% else %}
-    # HINT: Add your command-line arguments here.
-    # HINT: Add (default: %(default)s)') to help message if you have a default.
-    # HINT: Refer to https://docs.python.org/3/library/argparse.html
-    parser.add_argument('number', type=int, help='A number to test primality')
-    # HINT: Define an entry-point function to pass command-line arguments.
-    parser.set_defaults(function=prime_cli)
-    {%- endif %}
-    args = parser.parse_args()
-    if 'function' not in args:
-        parser.print_help()
-        sys.exit()
+    def parse_args(self):
+        {% if cookiecutter.command_line_interface|lower == 'yes (with subcommand)' %}
+        parser, subparser = self.prepare_subparser()
+        # HINT: Add your each subcommand and arguments to a new subparser.
+        # HINT: Refer to https://docs.python.org/3/library/argparse.html
+        sp1 = subparser.add_parser(
+            'prime',
+            description=prime_cli.__doc__.splitlines()[0],
+            help='Check if a number is prime.',
+            parents=[self.base_parser],
+            epilog=parser.epilog)
+        # HINT: Add (default: %(default)s)') to help message if you have a default.
+        sp1.add_argument('number', type=IntRange(10,100), help='A number to test.')
+        # HINT: For each subcommand, define an entry-point function.
+        sp1.set_defaults(function=prime_cli)
 
-    return _executeCommand(args)
+        # HINT: Create a new sub-parser with disctinct arguments.
+        sp2 = subparser.add_parser(
+            'fib',
+            description=fibonacci_cli.__doc__.splitlines()[0],
+            help='Check if a number is part of the fibonacci sequence.',
+            parents=[self.base_parser],
+            epilog=parser.epilog)
+        sp2.add_argument('number', type=int, help='A number to check.')
+        sp2.set_defaults(function=fibonacci_cli)
 
+        args = parser.parse_args(self.argv)
+        self._validate_subcommand(args, parser)
+        {% else %}
+        parser = self.prepare_parser()
+        # HINT: Add your command-line arguments here.
+        # HINT: Add (default: %(default)s)') to help message if you have a default.
+        # HINT: Refer to https://docs.python.org/3/library/argparse.html
+        parser.add_argument('number', type=int, help='A number to test primality')
+        # HINT: Define an entry-point function to pass command-line arguments.
+        parser.set_defaults(function=prime_cli)
+        args = parser.parse_args(self.argv)
+        {%- endif %}
+        return args
+
+
+def main(argv: list[str] | None = None) -> int | None:
+    epilog = 'Stephen Richer, (stephen.richer@proton.me)'
+    logFormat = '%(asctime)s - %(levelname)s - %(funcName)s - %(message)s'
+    args = CLIParser(argv, epilog, __version__).parse_args()
+    if args.verbose >= 2:
+        level = logging.DEBUG
+    elif args.verbose == 1:
+        level = logging.INFO
+    else:
+        level = logging.WARNING
+    logging.basicConfig(level=level, format=logFormat)
+    del args.verbose
+    del args.command
+    # Pop main function and excute script
+    function = args.__dict__.pop('function')
+    start = default_timer()
+    rc = function(**vars(args))
+    end = default_timer()
+    logging.info(f'Total execution time: {end - start:.3f} seconds.')
+    logging.shutdown()
+    return rc
+
+    
 # HINT: CLI entry-point function which takes command-line arguments.
 def prime_cli(number: int) -> bool:
     """ Check if an input number is prime. """
